20a0e24a2ba0ed34e540b25368753e176fc9894b
Dieguitogonzales <132518146+Dieguitogonzales@users.noreply.github.com>
2023-11-08 20:23:51 -0400

    NOMBRAR METODO

--- tarea 1/Program.cs
+++ tarea 1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("AREA CUADRADO");
             int resp = calcularAreaCuadrado(10);
             Console.WriteLine(resp);
         }
