fbed6ce8624b3f5b65181a94ade35bc3ac0ab0fe
Dieguitogonzales <132518146+Dieguitogonzales@users.noreply.github.com>
2023-11-21 10:48:05 -0400

    avance

---
+++ codigo facilito/codigofacilito#9/ClsAbsClientes.cs
@@ -0,0 +1,12 @@
+namespace Presentacion7
+ { 
+ public abstract class clsAbsClientes 
+ { 
+    public abstract int Id { get; set; } 
+    public abstract string Nombre { get; set; } 
+     public abstract string Clave { get; set; } 
+      public abstract string RFC { get; set; } 
+      public abstract int TipoRegimen { get; set; } 
+       public abstract string NombreContacto { get; set; } 
+       }
+ }
